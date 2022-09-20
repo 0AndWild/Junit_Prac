bf8776c0ec650e39499d2110b1cb8e37ea4272ef
박건영(Parkgunyoung) <104492837+0AndWild@users.noreply.github.com>
2022-09-21 02:22:03 +0900

    책등록_test 완료

--- src/test/java/com/junit/prac/domain/BookRepositoryTest.java
+++ src/test/java/com/junit/prac/domain/BookRepositoryTest.java
@@ -4,6 +4,7 @@ import com.junit.prac.repository.BookRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest //DB와 관련된 컴포넌트만 메모리에 로딩
 public class BookRepositoryTest {
@@ -14,7 +15,22 @@ public class BookRepositoryTest {
     //1. 책 등록
     @Test
     public void 책등록_Test(){
-        System.out.println("책등록_test 실행");
+        //given (데이터 준비)
+        String title = "Junit5";
+        String author = "건영";
+        Book book = Book.builder()
+                .title(title)
+                .author(author)
+                .build();
+
+        //when(테스트 실행)
+        //PS = persistence 즉, DB에 저장되어 영속화된
+        Book bookPS = bookRepository.save(book);
+
+        //then(검증)
+        assertEquals(title,bookPS.getTitle());
+        assertEquals(author,bookPS.getAuthor());
+
     }
     //2. 책 목록보기
 
