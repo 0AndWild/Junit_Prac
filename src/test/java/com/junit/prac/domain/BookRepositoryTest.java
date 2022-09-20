7ad7c9dc4b364f726324392977003fbe27653178
박건영(Parkgunyoung) <104492837+0AndWild@users.noreply.github.com>
2022-09-21 05:23:56 +0900

    책 목록보기, 책 한건 보기, @BeforeEach 를 활용한 데이터 준비 완료

--- src/test/java/com/junit/prac/domain/BookRepositoryTest.java
+++ src/test/java/com/junit/prac/domain/BookRepositoryTest.java
@@ -1,9 +1,14 @@
 package com.junit.prac.domain;
 
 import com.junit.prac.repository.BookRepository;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest //DB와 관련된 컴포넌트만 메모리에 로딩
@@ -11,6 +16,21 @@ public class BookRepositoryTest {
 
     @Autowired // DI
     private BookRepository bookRepository;
+    //@BeforeAll //테스트 시작전에 한번만 실행
+    @BeforeEach //각 테스트 시작전에 한번씩 실행
+    public void 데이터준비(){
+//        System.out.println("=====================================================");
+        String title = "Junit5";
+        String author = "건영";
+        Book book = Book.builder()
+                .title(title)
+                .author(author)
+                .build();
+        bookRepository.save(book);
+    }//트랜잭션이 종료 됐다면 말이안됨
+    //가정1 : [데이터준비() + 1 책등록] (T) , [데이터준비() + 2 책목록보기] (T) -> 책 목록보기 findAll() 사이즈 1 (검증완료)
+    //가정2 : [데이터준비() + 1 책등록 + 데이터준비() + 2 책목록보기] (T) -> 책 목록보기 findAll() 사이즈 2 (검증 실패)
+
 
     //1. 책 등록
     @Test
@@ -30,12 +50,39 @@ public class BookRepositoryTest {
         //then(검증)
         assertEquals(title,bookPS.getTitle());
         assertEquals(author,bookPS.getAuthor());
+    }//트랜잭션 종료 (저장된 데이터를 초기화함)
 
-    }
+    @Test
     //2. 책 목록보기
+    public void 책목록보기_test(){
+        // given
+        String title = "Junit5";
+        String author = "건영";
+
+        //when
+        List<Book> booksPS = bookRepository.findAll();
+        //@BeforeEach 트랜잭션 검증
+        System.out.println("========================"+ "사이즈 = " + booksPS.size()+ "==============================");
+
+        //then
+        assertEquals(title,booksPS.get(0).getTitle());
+        assertEquals(author,booksPS.get(0).getAuthor());
+    }//트랜잭션 종료 (저장된 데이터를 초기화함)
 
     //3. 책 한건 보기
+    @Test
+    public void 책한건보기_test(){
+        //given
+        String title = "Junit5";
+        String author = "건영";
 
+        //when
+        Book bookPS = bookRepository.findById(1L).get();
+
+        //then
+        assertEquals(title,bookPS.getTitle());
+        assertEquals(author,bookPS.getAuthor());
+    }
     //4. 책 수정
 
     //5. 책 삭제
