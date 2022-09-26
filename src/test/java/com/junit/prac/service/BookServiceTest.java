f59207bbc70f30d3e7585ef8530b95fe2d7fbb1b
박건영(Parkgunyoung) <104492837+0AndWild@users.noreply.github.com>
2022-09-26 21:05:59 +0900

    서비스 레이어 테스트 적용하기 - stub, mock, assertJ 완료

--- src/test/java/com/junit/prac/service/BookServiceTest.java
+++ src/test/java/com/junit/prac/service/BookServiceTest.java
@@ -3,36 +3,47 @@ package com.junit.prac.service;
 import com.junit.prac.dto.BookRespDto;
 import com.junit.prac.dto.BookSaveReqDto;
 import com.junit.prac.repository.BookRepository;
-import com.junit.prac.util.MailSenderStub;
+import com.junit.prac.util.MailSender;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import static org.assertj.core.api.Assertions.*;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
-@DataJpaTest
+@ExtendWith(MockitoExtension.class) //가짜환경을 만들어 @Mock 를 띄우고 @InjectMocks에 의존성 주입을 해줌
 public class BookServiceTest {
 
-    @Autowired
+    @InjectMocks
+    private BookService bookService;
+    @Mock
     private BookRepository bookRepository;
-    @Test //문제점 -> 서비스만 테스트하고 싶은데, 레포지토리 레이어가 함께 테스트 된다는 점.
+    @Mock
+    private MailSender mailSender;
+
+    @Test
     public void 책등록하기_테스트(){
         //given
         BookSaveReqDto dto = new BookSaveReqDto();
         dto.setTitle("Junit5");
         dto.setAuthor("건영");
 
-        //stub
-        MailSenderStub mailSenderStub = new MailSenderStub();
-        //가짜로 bookRepository 만들기 필요!
-
+        //stub(가설)
+        when(bookRepository.save(ArgumentMatchers.any())).thenReturn(dto.toEntity());
+        when(mailSender.send()).thenReturn(true);
         //when
-        BookService bookService = new BookService(bookRepository,mailSenderStub);
         BookRespDto bookRespDto = bookService.insertBook(dto);
 
         //then
-        assertEquals(dto.getTitle(),bookRespDto.getTitle());
-        assertEquals(dto.getAuthor(),bookRespDto.getAuthor());
+//        assertEquals(dto.getTitle(),bookRespDto.getTitle());
+//        assertEquals(dto.getAuthor(),bookRespDto.getAuthor());
+        /*AssertJ*/
+        assertThat(dto.getTitle()).isEqualTo(bookRespDto.getTitle());
+        assertThat(dto.getAuthor()).isEqualTo(bookRespDto.getAuthor());
     }
 
 
